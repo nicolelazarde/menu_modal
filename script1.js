62c82d3bb5ac5b418758165b4e28b955d52d817d
nicolelazarde <nicolelazarde.s@gmail.com>
2021-11-19 13:29:39 -0300

    subo archivos de menu sin terminar

---
+++ script1.js
@@ -0,0 +1,14 @@
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl)
+})
+
+var myOffcanvas = document.getElementById('myOffcanvas')
+var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas)
+
+var myOffcanvas = document.getElementById('myOffcanvas')
+myOffcanvas.addEventListener('hidden.bs.offcanvas', function () {
+  // do something...
+})
+
+
